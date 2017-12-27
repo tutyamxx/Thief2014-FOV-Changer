063a9798dc44d22e55cbeb6b1ec49d82ac04ae58
tutyamxx <tutygoo@gmail.com>
2017-12-27 17:33:56 +0000

    :100:
    Updated the code

--- Thief (2014) FOV Changer/Thief (2014) FOV Changer/Form1.cs
+++ Thief (2014) FOV Changer/Thief (2014) FOV Changer/Form1.cs
@@ -2,6 +2,7 @@
 
 using System;
 using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Windows.Forms;
 
@@ -10,34 +11,55 @@ namespace Thief__2014__FOV_Changer
     public partial class Form1 : Form
     {
         public int FOVBarValue = 0;
+        string RegValueName = "FOVCorrection";
 
         public Form1()
         {
             InitializeComponent();
         }
 
+        RegistryKey myKey = Registry.CurrentUser.OpenSubKey(@"Software\Eidos Montreal\Thief\Graphics", true);
+
         private void button1_Click(object sender, EventArgs e)
         {
-            int FOVBarValueConverted = fov_Bar.Value * 1000;
-
-            string ConvertFOVNumberToHex = FOVBarValueConverted.ToString("X");
-            string RegValueName = "FOVCorrection";
-
-            int FOVToIntAgain = int.Parse(ConvertFOVNumberToHex, System.Globalization.NumberStyles.HexNumber);
-
-            RegistryKey myKey = Registry.CurrentUser.OpenSubKey(@"Software\Eidos Montreal\Thief\Graphics", true);
-
             if (Registry.GetValue(myKey.ToString(), RegValueName, null) == null)
             {
                 MessageBox.Show("It seems you don't have Thief (2014) installed!" + Environment.NewLine + "Registry not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                Application.Exit();
             }
+
+            int FOVBarValueConverted = fov_Bar.Value * 1000;
+
+            string ConvertFOVNumberToHex = FOVBarValueConverted.ToString("X");
+            int FOVToIntAgain = int.Parse(ConvertFOVNumberToHex, System.Globalization.NumberStyles.HexNumber);
 
             myKey.SetValue(RegValueName, FOVToIntAgain, RegistryValueKind.DWord);
             MessageBox.Show("Added " + FOVBarValue + " (" + ConvertFOVNumberToHex + ")" + " to the base FOV!", "Done.", MessageBoxButtons.OK, MessageBoxIcon.Information);
         }
 
+        public static int FromHex(string value)
+        {
+            // strip the leading 0x
+            if (value.StartsWith("0x", StringComparison.OrdinalIgnoreCase))
+            {
+                value = value.Substring(2);
+            }
+            return Int32.Parse(value, NumberStyles.HexNumber);
+        }
+
         private void Form1_Load(object sender, EventArgs e)
         {
+            if (Registry.GetValue(myKey.ToString(), RegValueName, null) == null)
+            {
+                MessageBox.Show("It seems you don't have Thief (2014) installed!" + Environment.NewLine + "Registry not found!", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+
+                Application.Exit();
+            }
+
+            var ReadFOV = myKey.GetValue(RegValueName);
+
+            fov_Bar.Value = (Convert.ToInt32(ReadFOV) / 1000);
             FOVBarValue = fov_Bar.Value;
 
             lbl_FOV.Text = "Field of View (FOV)" + Environment.NewLine + "            " + FOVBarValue + "°";
